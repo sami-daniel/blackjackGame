c3e7b5947fd86f255d9512c38a10614b407924c1
sami-daniel <samidaniel125@gmail.com>
2023-04-22 00:08:14 -0300

    Adição Guna
    Adição da biblioteca guna

--- blackjackGame/Menu.Designer.cs
+++ blackjackGame/Menu.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(127)))), ((int)(((byte)(14)))));
+            this.ClientSize = new System.Drawing.Size(419, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Menu";
+            this.ShowIcon = false;
+            this.Text = "Blackjack";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
