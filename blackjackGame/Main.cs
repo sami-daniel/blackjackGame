892da3b8e990021755cca67a6c7cb4abe7079d65
sami-daniel <samidaniel125@gmail.com>
2023-05-08 23:56:35 -0300

    Shuffle deck.
    Criação do método para embaralhar as cartas.

--- blackjackGame/Main.cs
+++ blackjackGame/Main.cs
@@ -36,31 +36,52 @@ namespace blackjackGame
             string pastaImagem = "C:\\Users\\Sami\\Projetos\\blackjackGame\\blackjackGame\\Assets\\Baralho";
             foreach (string naipe in Naipes())
             {
-                foreach(string valor in ValorCarta())
+                foreach (string valor in ValorCarta())
                 {
-                    string nomeImagem = $"{valor.ToString().ToLower()}_de_{valor.ToString().ToLower()}.png";
+                    string nomeImagem = $"{valor.ToString().ToLower()}_de_{naipe.ToString().ToLower()}.png";
                     string caminhoImagem = Path.Combine(pastaImagem, nomeImagem);
 
                     deck.Add(new Carta(naipe, valor, caminhoImagem));
                 }
             }
+            Embaralhar(ref deck);
             return deck;
         }
-        public string[] Naipes()
+        private static void Embaralhar<T>(ref List<T> deck)
+        {
+            ///<summary>
+            ///Este metódo embaralha a lista (sempre Deck) passada como referencia.
+            ///Utiliza o algoritimo Fisher-Yates para aleatorizar os elementos da lista.
+            ///Para obter mais informações, consulte https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+            ///</summary>
+
+            Random rnd = new Random();
+            int n = deck.Count;
+            while(n > 1)
+            {
+                n--;
+                int k = rnd.Next(n+1);
+                T value = deck[k];
+                deck[k] = deck[n];
+                deck[n] = value;
+            }
+        }
+        private string[] Naipes()
         {
             string[] Naipe = { "Paus", "Copas", "Espadas", "Ouros" };
+    
             return Naipe;
         }
-        public string[] ValorCarta()
+        private string[] ValorCarta()
         {
             string[] ValorCarta = { "Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei" };
             return ValorCarta;
         }
         public class Carta : Deck
         {
-            public string Naipe { get; set; }
-            public int Valor { get; set; }
-            public string CaminhoImagem { get; set; }
+            public string Naipe { get; private set; }
+            public int Valor { get; private set; }
+            public string CaminhoImagem { get; private set; }
             public Carta(string naipe, string valor, string caminhoImagem)
             {
                 Naipe = naipe;
@@ -68,16 +89,16 @@ namespace blackjackGame
                 {
                     Valor = 1;
                 }
-                else if(valor == "Valete")
+                else if (valor == "Valete")
                 {
                     Valor = 10;
                 }
-                else if(valor == "Dama")
+                else if (valor == "Dama")
                 {
                     Valor = 10;
                 }
-                else if(valor == "Rei")
-                { 
+                else if (valor == "Rei")
+                {
                     Valor = 10;
                 }
                 else
